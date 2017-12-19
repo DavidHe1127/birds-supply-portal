d74400bc856bf3e4bf3efb1ce601ba3ffff1999a
David He <jialhe85@gmail.com>
2017-12-19 16:33:37 +1100

    Add products locally

--- src/components/Main/Products/index.js
+++ src/components/Main/Products/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { createFragmentContainer, graphql } from 'react-relay';
 
-import { Header, Search, Container } from 'semantic-ui-react';
+import { Header, Search, Container, Card } from 'semantic-ui-react';
+
+import src from 'images/elliot.jpg';
 
 // import styled from 'styled-components';
 
@@ -13,6 +15,22 @@ class Products extends Component {
         <Header>
           <Search />
         </Header>
+        <Container>
+          <Card.Group itemsPerRow={4}>
+            <Card color="red" image={src} />
+            <Card color="orange" image={src} />
+            <Card color="yellow" image={src} />
+            <Card color="olive" image={src} />
+            <Card color="green" image={src} />
+            <Card color="teal" image={src} />
+            <Card color="blue" image={src} />
+            <Card color="violet" image={src} />
+            <Card color="purple" image={src} />
+            <Card color="pink" image={src} />
+            <Card color="brown" image={src} />
+            <Card color="grey" image={src} />
+          </Card.Group>
+        </Container>
       </Container>
     );
   }
