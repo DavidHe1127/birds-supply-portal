5491c2cb04b5ea2af42a1ff0f97455b49ecbc003
David He <jialhe85@gmail.com>
2017-12-28 16:49:36 +1100

    Form

--- src/containers/Main/Products/Form.js
+++ src/containers/Main/Products/Form.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { Form, Header, Image } from 'semantic-ui-react';
+import { Form, Header, Image, Select } from 'semantic-ui-react';
 import avatar from 'images/parrot_avatar.svg';
 
 const Avatar = styled.div`
   margin-bottom: 10px;
   background: #f2f3f4;
 `;
+
+const options = [
+  { key: 'm', text: 'Male', value: 'male' },
+  { key: 'f', text: 'Female', value: 'female' },
+];
+
 
 export default class FormContainer extends React.Component {
   render() {
@@ -16,33 +22,19 @@ export default class FormContainer extends React.Component {
         <Avatar>
           <Image circular centered src={avatar} size="small" />
         </Avatar>
-        <Form.Group widths="equal">
-          <Form.Input
-            id="form-subcomponent-shorthand-input-first-name"
-            label="Name"
-            placeholder="Name"
-          />
-          <Form.Input
-            id="form-subcomponent-shorthand-input-last-name"
-            label="Size"
-            placeholder="Size"
-          />
-          <Form.Input
-            id="form-subcomponent-shorthand-input-last-name"
-            label="Mass"
-            placeholder="Mass in g"
-          />
-          <Form.Input
-            id="form-subcomponent-shorthand-input-last-name"
-            label="Distribution"
-            placeholder="Distribution"
-          />
-          <Form.Input
-            id="form-subcomponent-shorthand-input-last-name"
-            label="Lifespan"
-            placeholder="Lifespan"
-          />
-        </Form.Group>
+        <Form.Input
+          id="form-subcomponent-shorthand-input-first-name"
+          label="price"
+          placeholder="Price"
+        />
+        <Form.Input
+          id="form-subcomponent-shorthand-input-first-name"
+          label="qty"
+          placeholder="Qty"
+          type="number"
+          max="100"
+        />
+        <Form.Field control={Select} label='Parrot' options={options} placeholder='Gender' />
       </Form>
     );
   }
