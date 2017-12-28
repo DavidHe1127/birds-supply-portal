eb83f918f8b135286743eee95f4e56fd6dbeea06
David He <jialhe85@gmail.com>
2017-12-28 16:23:55 +1100

    WIP form

--- src/containers/Main/Products/Form.js
+++ src/containers/Main/Products/Form.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { Form, Header, Image } from 'semantic-ui-react';
-import avatar from 'images/parrot_avatar.png';
+import avatar from 'images/parrot_avatar.svg';
 
 const Avatar = styled.div`
   margin-bottom: 10px;
-  background: #e6f2ff;
+  background: #f2f3f4;
 `;
 
 export default class FormContainer extends React.Component {
@@ -19,13 +19,28 @@ export default class FormContainer extends React.Component {
         <Form.Group widths="equal">
           <Form.Input
             id="form-subcomponent-shorthand-input-first-name"
-            label="First name"
-            placeholder="First name"
+            label="Name"
+            placeholder="Name"
           />
           <Form.Input
             id="form-subcomponent-shorthand-input-last-name"
-            label="Last name"
-            placeholder="Last name"
+            label="Size"
+            placeholder="Size"
+          />
+          <Form.Input
+            id="form-subcomponent-shorthand-input-last-name"
+            label="Mass"
+            placeholder="Mass in g"
+          />
+          <Form.Input
+            id="form-subcomponent-shorthand-input-last-name"
+            label="Distribution"
+            placeholder="Distribution"
+          />
+          <Form.Input
+            id="form-subcomponent-shorthand-input-last-name"
+            label="Lifespan"
+            placeholder="Lifespan"
           />
         </Form.Group>
       </Form>
