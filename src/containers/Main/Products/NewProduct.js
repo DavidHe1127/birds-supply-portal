9b7b5046f647c4d61c185f9284e6833215dfc54b
David He <jialhe85@gmail.com>
2018-01-17 23:31:56 +1100

    Edit product wip

--- src/containers/Main/Products/NewProduct.js
+++ src/containers/Main/Products/NewProduct.js
@@ -4,21 +4,44 @@ import environment from 'Environment';
 
 import NewProduct from 'components/Main/Products/NewProduct';
 
-const parrotsToProductQuery = graphql`
-  query NewProductContainerQuery {
-    parrotsToProduct {
-      ...NewProduct_parrotsToProduct
+const queries = {
+  newProduct: graphql`
+    query NewProductContainerQuery {
+      parrotsToProduct {
+        ...NewProduct_parrotsToProduct
+      }
     }
-  }
-`;
+  `,
+  editProduct: graphql`
+    query NewProductContainerQuery($id: ID!) {
+      product(id: $id) {
+        price
+        qty
+        parrot {
+          code
+        }
+      }
+    }
+  `
+};
 
 export default class NewProductContainer extends React.Component {
-  render () {
+  variables = {
+    id: null,
+  }
+
+  render() {
+    this.variables.id = this.props.match.params.productId;
+
+    const query = !!this.variables.id ? queries.editProduct : queries.newProduct;
+
     return (
       <QueryRenderer
         environment={environment}
-        query={parrotsToProductQuery}
+        query={query}
+        variables={this.variables}
         render={({ error, props }) => {
+          console.log(props);
           if (error) {
             return <div>{error.message}</div>;
           } else if (props) {
