7fc3a70df110ee17f7ba2b159b1c30911dffa727
David He <jialhe85@gmail.com>
2018-02-14 22:55:10 +1100

    WIP

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -12,31 +12,25 @@ import { Link } from 'react-router-dom';
 import './styles/index.css';
 
 const Login = () => (
-  <div className="login">
-        <Header as="h2" color="#031727" textAlign="center">
-          Log into your account
-        </Header>
-        <Form size="large">
-          <Segment stacked>
-            <Form.Input
-              fluid
-              label="Username"
-              placeholder="abc@abc.com"
-            />
-            <Form.Input
-              fluid
-              label="Password"
-              type="password"
-            />
-            <Button color="teal" fluid size="large">
-              Login
-            </Button>
-          </Segment>
-        </Form>
-        <Message>
-          New to us? <Link to='/signup'>Sign Up</Link>
-        </Message>
-  </div>
+  <Grid centered verticalAlign="middle" columns={4} className="login">
+    <Grid.Column>
+      <Header as="h2" color="#031727" textAlign="center">
+        Log into your account
+      </Header>
+      <Form size="large">
+        <Segment stacked>
+          <Form.Input fluid label="Username" placeholder="abc@abc.com" />
+          <Form.Input fluid label="Password" type="password" />
+          <Button color="teal" fluid size="large">
+            Login
+          </Button>
+        </Segment>
+      </Form>
+      <Message>
+        New to us? <Link to="/signup">Sign Up</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
 );
 
 export default Login;
