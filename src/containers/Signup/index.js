f38239e3335db310e3deb8b46a7da88d998b5868
David He <jialhe85@gmail.com>
2018-02-15 15:57:35 +1100

    Vertically align login form

--- src/containers/Signup/index.js
+++ src/containers/Signup/index.js
@@ -1,51 +1,39 @@
 import React from 'react';
-import {
-  Button,
-  Form,
-  Grid,
-  Header,
-  Message,
-  Segment
-} from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import {Button, Form, Grid, Header, Message, Segment} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+
+import './styles/index.css';
 
 const Signup = () => (
-  <div className="login">
-    <Grid textAlign="center" style={{ height: '100%' }} verticalAlign="middle">
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as="h2" color="#031727" textAlign="center">
-          Log into your account
-        </Header>
-        <Form size="large">
-          <Segment stacked>
-            <Form.Input
-              fluid
-              placeholder="abc@abc.com"
-            />
-            <Form.Input
-              fluid
-              type="password"
-              placeholder="Require min length 8 & lowercase letters"
-            />
-            <Form.Input
-              fluid
-              placeholder="First Name"
-            />
-            <Form.Input
-              fluid
-              placeholder="Last Name"
-            />
-            <Button color="teal" fluid size="large">
-              Sign Up Today
-            </Button>
-          </Segment>
-        </Form>
-        <Message>
-          Already have an account? <Link to="/login">Log In</Link>
-        </Message>
-      </Grid.Column>
-    </Grid>
-  </div>
+  <Grid
+    textAlign="center"
+    container
+    verticalAlign="middle"
+  >
+    <Grid.Column style={{maxWidth: 450}}>
+      <Header as="h2" color="#031727" textAlign="center">
+        Log into your account
+      </Header>
+      <Form size="large">
+        <Segment stacked>
+          <Form.Input fluid placeholder="abc@abc.com" />
+          <Form.Input
+            fluid
+            type="password"
+            placeholder="Require min length 8 & lowercase letters"
+          />
+          <Form.Input fluid placeholder="First Name" />
+          <Form.Input fluid placeholder="Last Name" />
+          <Button color="teal" fluid size="large">
+            Sign Up Today
+          </Button>
+        </Segment>
+      </Form>
+      <Message>
+        Already have an account? <Link to="/login">Log In</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
 );
 
 export default Signup;
