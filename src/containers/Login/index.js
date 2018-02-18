78f1e5bc29bb543725652693586f9c0a2720c848
David He <jialhe85@gmail.com>
2018-02-18 16:14:39 +1100

    Add css variable

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,36 +1,47 @@
 import React from 'react';
-import {
-  Button,
-  Form,
-  Grid,
-  Header,
-  Message,
-  Segment
-} from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import {Button, Form, Grid, Header, Message, Segment} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
 
 import './styles/index.css';
 
-const Login = () => (
-  <Grid centered verticalAlign="middle" columns={4} className="login">
-    <Grid.Column>
-      <Header as="h2" color="#031727" textAlign="center">
-        Log into your account
-      </Header>
-      <Form size="large">
-        <Segment stacked>
-          <Form.Input fluid label="Username" placeholder="abc@abc.com" />
-          <Form.Input fluid label="Password" type="password" />
-          <Button color="teal" fluid size="large">
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        New to us? <Link to="/signup">Sign Up</Link>
-      </Message>
-    </Grid.Column>
-  </Grid>
-);
+export default class Login extends React.Component {
 
-export default Login;
+  state = {
+    username: null,
+    password: null
+  }
+
+  onChange = e => {
+    this.setState = {
+      [e.target.name]: e.target.value
+    };
+  }
+
+  onSubmit = e => {
+
+  }
+
+  render() {
+    return (
+      <Grid centered verticalAlign="middle" columns={4} className="login">
+        <Grid.Column>
+          <Header as="h2" textAlign="center">
+            Log into your account
+          </Header>
+          <Form size="large" onSubmit={this.onSubmit}>
+            <Segment stacked>
+              <Form.Input fluid name="username" label="Username" placeholder="abc@abc.com" onChange={this.onChange} />
+              <Form.Input fluid name="password" label="Password" type="password" onChange={this.onChange} />
+              <Button color="yellow" fluid size="large">
+                Login
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            New to us? <Link to="/signup">Sign Up</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
