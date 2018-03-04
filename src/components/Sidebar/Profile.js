4e73b1e55bb3f5ea0706a03a41e723c65b949850
David He <jialhe85@gmail.com>
2018-03-04 21:57:52 +1100

    Add profile option to the sidebar

--- src/components/Sidebar/Profile.js
+++ src/components/Sidebar/Profile.js
@@ -1,18 +1,51 @@
 import React from 'react';
-import { Image } from 'semantic-ui-react';
+import { Icon, Popup, List } from 'semantic-ui-react';
 import styled from 'styled-components';
 
-import profile from 'images/profile.png';
+import './styles/index.css';
 
 const Wrapper = styled.div`
   position: absolute;
   bottom: 10px;
-  left: 10px;
+  left: 15px;
+`;
+
+const Option = styled.a`
+  color: #000;
+  font-size: 14px;
+
+  &:hover {
+    color: var(--amber);
+  }
 `;
 
 const Profile = () => (
   <Wrapper>
-    <Image src={profile} avatar />
+    <Popup
+      trigger={<Icon name="user circle" size="big" className="profile" />}
+      position="right center"
+      on="click"
+    >
+      <Popup.Content>
+        <List divided verticalAlign="middle" size="big">
+          <List.Item>
+            <List.Header>
+              <Option>Settings</Option>
+            </List.Header>
+          </List.Item>
+          <List.Item>
+            <List.Header>
+              <Option>Users</Option>
+            </List.Header>
+          </List.Item>
+          <List.Item>
+            <List.Header>
+              <Option>Logout</Option>
+            </List.Header>
+          </List.Item>
+        </List>
+      </Popup.Content>
+    </Popup>
   </Wrapper>
 );
 
