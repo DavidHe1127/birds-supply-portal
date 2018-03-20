4f5adaa93bd6548f626914864a3023044c7fa9bd
David He <jialhe85@gmail.com>
2018-03-20 22:05:35 +1100

    Add missing routes

---
+++ src/components/Main/Products/Routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Products from 'containers/Main/Products';
+import NewProduct from 'containers/Main/Products/NewProduct';
+import EditProduct from 'containers/Main/Products/EditProduct';
+
+export default () => (
+  <Switch>
+    <Route exact path='/products' component={Products} />
+    <Route path='/products/new' component={NewProduct} />
+    <Route path='/products/:productId/edit' component={EditProduct} />
+  </Switch>
+);
