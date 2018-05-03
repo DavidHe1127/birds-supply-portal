d8f0be4b28e94d52f30cd77866f6b13d626524ae
David He <jialhe85@gmail.com>
2018-05-03 14:12:29 +1000

    CP done image preload

--- src/helpers/IronImage.js
+++ src/helpers/IronImage.js
@@ -2,21 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 import buildUrl from 'utils/buildUrl';
-import { Image as Placeholder } from 'semantic-ui-react';
+import { Image } from 'semantic-ui-react';
 
 import placeholder from 'images/parrot_avatar.jpg';
 
-  // min-height: 266px;
-  // min-width: 240px;
-const Wrapper = styled.div`
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  background-size: 100% 100%;
-  background-image: url(${props => props.placeholder});
-`;
-
-const Image = styled.img`
+const ActualImage = styled.img`
   z-index: 2;
   top: 0;
   right: 0;
@@ -28,58 +18,39 @@ const Image = styled.img`
   transition: opacity 1s ease;
 `;
 
-let image;
-
 export default class IronImage extends React.Component {
   state = {
-    onImageLoad: false,
-    image: null
-  }
+    onImageLoad: false
+  };
 
-  componentDidMount() {
-    image = new Image();
+  onImageLoad = () => {
+    this.setState({onImageLoad: true});
+  };
 
-    const { w, h, q, f } = this.props;
-
+  render() {
+    const {w, h, q, f} = this.props;
     const url = buildUrl.imageProcessor({
       url: process.env.REACT_APP_IMAGE_PROCESSOR_API,
       queries: {
         w,
         h,
         q,
-        f
-      }
+        f,
+      },
     });
 
-    image.onLoad = () => {
-      console.log('ccvv');
-      this.onImageLoad();
-    };
-
-    image.src = url;
-  }
-
-  onImageLoad = () => {
-    this.setState({ onImageLoad: true });
-  }
-
-  render() {
-    if (!this.state.onImageLoad) {
-      return <Placeholder src={placeholder}></Placeholder>
-    }
-    console.log('x');
-
-    return image;
-
     return (
-      <Wrapper placeholder={placeholder}>
-        <Image
+      <Image>
+        <ActualImage
           opacity={this.state.onImageLoad}
           alt={'image failed to load'}
           onLoad={this.onImageLoad}
+          src={url}
         />
-      </Wrapper>
+        {!this.state.onImageLoad && <Image
+          src={placeholder}
+        />}
+      </Image>
     );
   }
 }
-          // src={url}
