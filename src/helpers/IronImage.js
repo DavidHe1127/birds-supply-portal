69e16d64587f1aab74b313174bacb20270fbbda7
David He <jialhe85@gmail.com>
2018-05-19 00:53:18 +1000

    Fix issue where image fetching is fucked up

--- src/helpers/IronImage.js
+++ src/helpers/IronImage.js
@@ -8,27 +8,25 @@ import placeholder from 'images/parrot_avatar.jpg';
 
 const ActualImage = styled.img`
   z-index: 2;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  opacity: ${props => props.opacity || 0};
+  opacity: ${props => props.opacity};
   transition: opacity 1s ease;
+  display: ${props => props.display} !important;
 `;
 
 export default class IronImage extends React.Component {
   state = {
     onImageLoad: false
-  };
+  }
 
   onImageLoad = () => {
     this.setState({onImageLoad: true});
-  };
+  }
 
-  render() {
-    const {w, h, q, f} = this.props;
+  computeUrl = (w, h, q, f) => {
+    if (this.computeUrl.url) {
+      return this.computeUrl.url;
+    }
+
     const url = buildUrl.imageProcessor({
       url: process.env.REACT_APP_IMAGE_PROCESSOR_API,
       queries: {
@@ -39,13 +37,25 @@ export default class IronImage extends React.Component {
       },
     });
 
+    this.computeUrl.url = url;
+
+    return url;
+  }
+
+  render() {
+    const {w, h, q, f} = this.props;
+
+    const opacity = this.state.onImageLoad ? 1 : 0;
+    const display = this.state.onImageLoad ? 'block' : 'none';
+
     return (
       <Image>
         <ActualImage
-          opacity={this.state.onImageLoad}
+          opacity={opacity}
           alt={'image failed to load'}
           onLoad={this.onImageLoad}
-          src={url}
+          src={this.computeUrl(w, h, q, f)}
+          display={display}
         />
         {!this.state.onImageLoad && <Image
           src={placeholder}
