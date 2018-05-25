3ee15a860dcc0851a54d540a59c9d77cb08a2437
David He <jialhe85@gmail.com>
2018-05-25 21:57:14 +1000

    Add eager annotation to prevent extra request to fetch file chunk

--- src/queries/index.js
+++ src/queries/index.js
@@ -2,6 +2,8 @@ import { fetchQuery } from 'relay-runtime';
 
 import environment from 'Environment';
 
-const query = (q, variables) => import(`./${q}`).then(module => fetchQuery(environment, module.default, variables));
+// eager will enforce webpack to include file chunk in the parent chunk without creating a separate one
+// as a result, no additional request to fetch that chunk is fired off
+const query = (q, variables) => import(/* webpackMode: "eager" */`./${q}`).then(module => fetchQuery(environment, module.default, variables));
 
 export default query;
