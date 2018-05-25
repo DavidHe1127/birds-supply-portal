b470dcb9c9d2d330f357be33515602bc5f39fbb7
David He <jialhe85@gmail.com>
2018-05-25 14:19:43 +1000

    Code refactoring

--- src/queries/index.js
+++ src/queries/index.js
@@ -2,8 +2,6 @@ import { fetchQuery } from 'relay-runtime';
 
 import environment from 'Environment';
 
-const query = (q, variables) => import(`./${q}`).then(module => {
-  return fetchQuery(environment, module.default, variables);
-});
+const query = (q, variables) => import(`./${q}`).then(module => fetchQuery(environment, module.default, variables));
 
 export default query;
