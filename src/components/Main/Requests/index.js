64a93a7d87f29b0571c867a2bc033f1956eed0dc
David He <jialhe85@gmail.com>
2018-05-29 15:47:01 +1000

    WIP

--- src/components/Main/Requests/index.js
+++ src/components/Main/Requests/index.js
@@ -5,6 +5,7 @@ import {createFragmentContainer, graphql} from 'react-relay';
 
 import RequestTableHeader from './TableHeader';
 import RequestTableRow from './TableRow';
+import setRequestStatusMutation from 'mutations/setRequestStatusMutation';
 
 // const _REQUESTS = [{
 //   reqId: '1jkl',
@@ -27,7 +28,34 @@ import RequestTableRow from './TableRow';
 // }];
 
 class Requests extends React.Component {
-  onNewRequestClick = e => this.props.history.push('/requests/new');
+
+  state = {
+    actionRequestId: null
+  }
+
+  onNewRequestClick = e => this.props.history.push('/requests/new')
+
+  onApproveClick = e => {
+    setRequestStatusMutation({
+      id: this.status.id,
+      status: 'approved'
+    }, this.onApproved);
+  }
+
+  onRejectClick = e => {
+    setRequestStatusMutation({
+      id: this.status.id,
+      status: 'rejected'
+    }, this.onRejected);
+  }
+
+  onApproved = e => {
+
+  }
+
+  onRejected = e => {
+
+  }
 
   render() {
     const rows = this.props.viewer.requests.edges.map(x => (
