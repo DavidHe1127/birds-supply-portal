f88e41634c4717f02d110235ce426d8c17800014
David He <jialhe85@gmail.com>
2018-06-07 12:55:50 +1000

    Add polling

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,12 +1,33 @@
-const upload = ({ url, file, token }) => {
+const upload = ({url, file, token}) => {
   return fetch(url, {
     headers: {
       'Content-Type': file.type,
-      authorization: token
+      authorization: token,
     },
     method: 'PUT',
-    body: file
+    body: file,
   });
 };
 
-export { upload };
+const poll = (fn, timeout, interval) => {
+  const endTime = Number(new Date()) + (timeout || 2000);
+  interval = interval || 100;
+
+  const checkCondition = (resolve, reject) => {
+    // If the condition is met, we're done!
+    var result = fn();
+    if (result) {
+      resolve(result);
+    } else if (Number(new Date()) < endTime) {
+      // If the condition isn't met but the timeout hasn't elapsed, go again
+      setTimeout(checkCondition, interval, resolve, reject);
+    } else {
+      // Didn't match and too much time, reject!
+      reject(new Error('timed out for ' + fn + ': ' + arguments));
+    }
+  };
+
+  return new Promise(checkCondition);
+};
+
+export {upload, poll};
